2d06032f53d1be3601e0980ce8f7c0bbb0df2668
mycodebusness <167857252+mycodebusness@users.noreply.github.com>
2024-04-23 13:25:37 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MyCodeBusness",
+  description:
+    "un site d'apprentissage des bases de la programmations multiplateformes et de maitrise de la souris, du clavier et.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
