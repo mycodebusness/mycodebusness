9bf3b78493b3252d3ff3ee600f8a18d88fed5492
mycodebusness <167857252+mycodebusness@users.noreply.github.com>
2024-05-15 10:51:03 +0200

    Add files via upload

---
+++ app/dashboard/challenge/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold px-3 mt-3">Challenge</h2>
+    </div>
+  );
+}
+
+export default Page;
---
+++ app/ui/compteur.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+export default function Compteur({ dateInscrit }) {
+  const ecouler = new Date(dateInscrit).getTime();
+  const [day, setDay] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const dateNow = Date?.now();
+      const dateDif = (dateNow - ecouler) / 1000;
+      const d = parseInt(dateDif / 86400);
+      const day_dif = dateDif % 86400;
+      const h = parseInt(day_dif / 3600);
+      const hour_dif = day_dif % 3600;
+      const m = parseInt(hour_dif / 60);
+      const s = parseInt(hour_dif % 60);
+      setDay(d);
+      setHour(h);
+      setMin(m);
+      setSec(s);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  return (
+    <>
+      <span>{day?.toString()}j</span>
+      <span>{hour?.toString()}h</span>
+      <span>{min?.toString()}m</span>
+      <span>{sec?.toString()}s</span>
+    </>
+  );
+}
