746bc720f3e2226cb8424f9a8bd2ea6f0aee632f
mycodebusness <167857252+mycodebusness@users.noreply.github.com>
2024-05-15 10:45:07 +0200

    Delete app directory

--- app/lib/serverSesion.ts
+++
@@ -1,6 +0,0 @@
-import { authConfig } from "@/pages/api/auth/[...nextauth]";
-import { getServerSession } from "next-auth";
-
-export async function serverSesion() {
-  return await getServerSession(authConfig);
-}
