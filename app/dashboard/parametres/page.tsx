029d0745bcfeab40dff4aac119a922ed392ddc2e
mycodebusness <167857252+mycodebusness@users.noreply.github.com>
2024-05-20 07:09:51 +0200

    Update page.tsx

--- app/dashboard/parametres/page.tsx
+++ app/dashboard/parametres/page.tsx
@@ -1,15 +1,15 @@
-// import { serverSesion } from "@/app/lib/serverSesion";
+import { serverSesion } from "@/app/lib/serverSesion";
 import { Metadata } from "next";
 import FormParam from "./formParam";
+import { getUniqueProfileForUser } from "@/app/lib/fetchData";
 export const metadata: Metadata = {
   title: "parametres",
 };
 async function Page() {
-  // const session = await serverSesion();
-  // const id = session?.user.id || "";
-  const id = "";
-  // const profile = (await getUniqueProfileForUser(id)) || [];
-  const profile = [];
+  const session = await serverSesion();
+  const id = session?.user?.id ?? "";
+  const profile = await getUniqueProfileForUser(id);
+
   return <FormParam userId={id} profile={profile} />;
 }
 
